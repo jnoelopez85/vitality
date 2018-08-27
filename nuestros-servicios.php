a0a59bdea0f9c8269c0e32a39c27fd89394b811b
Noé López <jnoelopez85@gmail.com>
2018-08-27 11:49:51 -0500

    Texto en alt de imagenes

--- nuestros-servicios.php
+++ nuestros-servicios.php
@@ -21,7 +21,7 @@
 			<div class='row'>
 				<div class='col-md-12'>
 					<br>
-					<h1 class='center'><img class="titulo-icon" src='images/Icono-Servicios.png' alt='Valoración inicial vitality'> NUESTROS SERVICIOS</h1>
+					<h1 class='center'><img class="titulo-icon" src='images/Icono-Servicios.png' alt='Icono Nuestros Servicios Vitalitymexico'> NUESTROS SERVICIOS</h1>
 				</div>
 			</div>
 		</div>
@@ -35,7 +35,7 @@
 						<div class='col-md-6 col-sm-6'>
 							<div class=' animate-box' data-animate-effect='fadeIn'>
 									<br>
-									<img src='images/CUIDADOS-ASISTENCIALES.jpg' class='img-responsive' alt='Servicio de acompañamiento hospitalario o atención domiciliaria Mérida Yucatán'>
+									<img src='images/CUIDADOS-ASISTENCIALES.jpg' class='img-responsive' alt='Cuidados y asistencia a domicilio Mérida Yucatán'>
 									<br>
 							</div>
 						</div>
@@ -45,19 +45,19 @@
 								<br>
 								<br>
 								<br>
-								<h2><img class="titulo-icon" src='images/icono-asistencia-en-el-hogar.png' alt='Valoración inicial vitality'> ASISTENCIA EN EL HOGAR DE ENFERMERAS O CUIDADORAS (O)</h2>
+								<h2><img class="titulo-icon" src='images/icono-asistencia-en-el-hogar.png' alt='Enfermera a domicilio merida yucatan'> ASISTENCIA EN EL HOGAR DE ENFERMERAS O CUIDADORAS (O)</h2>
 								<ul>
 									<li>Acompañamiento de enfermería o cuidadoras asistenciales dependiendo de las necesidades del paciente</li>
 									<li>Contamos con turnos de 8, 12 y 24 horas.</li>
 									<li>Incluye visita mensual de apoyo psicológico.</li>
 								</ul>
-								<h2><img class="titulo-icon" src='images/icono-acompanamiento-hospitalario.png' alt='Valoración inicial vitality'> ACOMPAÑAMIENTO HOSPITALARIO</h2>
+								<h2><img class="titulo-icon" src='images/icono-acompanamiento-hospitalario.png' alt='Acompañamiento a clinicas y hospitales'> ACOMPAÑAMIENTO HOSPITALARIO</h2>
 								<ul>
 									<li>En situaciones de hospitalización se envía a un enfermero(a) a acompañar y vigilar el progreso del paciente.</li>
 								</ul>
 								<br>
 								<i>&emsp; &emsp; Enfermera (o) / Cuidadora</i>
-								<p>&emsp; &emsp;&nbsp;&nbsp; <a href='nuestros-servicios.php' title='Servicios y compromiso con el adulto mayor' class='btn btn-primary btn-lg'>Actividades por perfil</a></p>
+								<p>&emsp; &emsp;&nbsp;&nbsp; <a href='actividades-servicios-enfermera-cuidadora-vitality-adultos-mayores.pdf' title='Descargar archivo PDF' class='btn btn-primary btn-lg'>Actividades por perfil</a></p>
 							</div>
 						</div>
 				</div>
@@ -74,7 +74,7 @@
 						<div class='col-md-6 col-sm-6'>
 							<div class='animate-box' data-animate-effect='fadeIn'>
 								<br>
-								<h2><img class="titulo-icon-fisio" src='images/icono-fisioterapia.png' alt='Valoración inicial vitality'> FISIOTERAPIA MANUAL</h2>
+								<h2><img class="titulo-icon-fisio" src='images/icono-fisioterapia.png' alt='Fisioterapia manual vitality'> FISIOTERAPIA MANUAL</h2>
 								<p>Orientado a pacientes con: EVC, Fracturas, Post operatorios, Parkinson, problemas de espalda, ciática o lumbar, fibromialgia, hombro doloroso, dolor en cervicales, dolor de rodillas y/o artritis. O toda aquella persona con poca o nula actividad física</p>								
 								<h2>OBJETIVOS</h2>
 								<ul class='vineta_fisio'>
@@ -84,14 +84,14 @@
 									<li>Alivio del dolor agudo post-traumático y post-quirúrgico.</li>
 								</ul>
 								
-								<img class='pull-left' src='images/icono-fisioterapia-hogar.png' alt='Valoración inicial vitality' style='width: 2.3em;'>
+								<img class='pull-left' src='images/icono-fisioterapia-hogar.png' alt='Fisioterapia a domicilio' style='width: 2.3em;'>
 								<p>Las sesiones de Fisioterapia son impartidas en el hogar para la mayor comodidad y seguridad del paciente.</p>
 							</div>
 						</div>
 						<div class='col-md-6 col-sm-6'>
 							<div class=' animate-box' data-animate-effect='fadeIn'>
 									<br>
-									<img src='images/CUIDADOS-POST-OPERATORIOS.jpg' class='img-responsive' alt='Servicio de acompañamiento hospitalario o atención domiciliaria Mérida Yucatán'>
+									<img src='images/CUIDADOS-POST-OPERATORIOS.jpg' class='img-responsive' alt='Cuidados post operatorios Mérida Yucatán'>
 									<br>
 							</div>
 						</div>
@@ -107,7 +107,7 @@
 						<div class='col-md-6 col-sm-6'>
 							<div class=' animate-box' data-animate-effect='fadeIn'>
 									<br>
-								<img src='images/CUIDADOS-BIENESTAR-EMOCIONAL.jpg' class='img-responsive' alt='Servicio de acompañamiento hospitalario o atención domiciliaria Mérida Yucatán'>
+								<img src='images/CUIDADOS-BIENESTAR-EMOCIONAL.jpg' class='img-responsive' alt='Cuidados bienestar emocional en casa Mérida Yucatán'>
 									<br>
 
 							</div>
@@ -115,13 +115,13 @@
 						<div class='col-md-6 col-sm-6'>
 							<div class='animate-box' data-animate-effect='fadeIn'>
 								<br>
-								<h2><img class="titulo-icon" src='images/icono-psicoterapia.png' alt='Valoración inicial vitality'> PSICOTERAPIA PARA EL PACIENTE Y/O SUS FAMILIARES</h2>
+								<h2><img class="titulo-icon" src='images/icono-psicoterapia.png' alt='Psicoterapia Vitalitymexico'> PSICOTERAPIA PARA EL PACIENTE Y/O SUS FAMILIARES</h2>
 								<p>Trabajamos con el paciente y/o familiares para ayudarlo a cambiar pensamientos, sentimientos o conductas que lo aquejan; a través de estrategias de intervención cognitiva, afectivas, conductuales o sistemáticas que trabajan para el bienestar, crecimiento personal y desarrollo ocupacional del paciente.</p>
-								<h2><img class="titulo-icon" src='images/icono-terapia-ocupacional.png' alt='Valoración inicial vitality'> PROGRAMAS DE TERAPIA OCUPACIONAL</h2>
+								<h2><img class="titulo-icon" src='images/icono-terapia-ocupacional.png' alt='Terapia ocupacional Vitalitymexico'> PROGRAMAS DE TERAPIA OCUPACIONAL</h2>
 								<p>El objetivo de la terapia ocupacional es ayudar a la persona a que sea más independiente al realizar sus tareas cotidianas.</p>
-								<h2><img class="titulo-icon" src='images/icono-estimulacion-cognitiva.png' alt='Valoración inicial vitality'> PROGRAMAS DE ESTIMULACIÓN COGNITIVA</h2>
+								<h2><img class="titulo-icon" src='images/icono-estimulacion-cognitiva.png' alt='Estimulacion Cognitiva Vitalitymexico'> PROGRAMAS DE ESTIMULACIÓN COGNITIVA</h2>
 								<p>Ejercicios y actividades que tienen como objetivo el estimular, mejorar y mantener las capacidades y funcionamiento cognitivo existente y ralentizar el deterioro que se produce.</p>
-								<h2><img class="titulo-icon" src='images/icono-acompanamiento-tanatologico.png' alt='Valoración inicial vitality'> ACOMPAÑAMIENTO TANATOLÓGICO</h2>
+								<h2><img class="titulo-icon" src='images/icono-acompanamiento-tanatologico.png' alt='Ayuda Tanatológica Vitalitymexico'> ACOMPAÑAMIENTO TANATOLÓGICO</h2>
 								<p>Trabajamos con las personas y sus familiares que han sufrido una pérdida, asistiéndolos en las diferentes etapas del duelo y ayudándolos a comprenderla, enfrentarla y superarla</p>
 						</div>
 						
@@ -159,7 +159,7 @@
 						<div class='col-md-6 col-sm-6'>
 							<div class=' animate-box' data-animate-effect='fadeIn'>
 									<br>
-								<img src='images/CUIDADOS-DE-URGENCIA.jpg' class='img-responsive' alt='Servicio de acompañamiento hospitalario o atención domiciliaria Mérida Yucatán'>
+								<img src='images/CUIDADOS-DE-URGENCIA.jpg' class='img-responsive' alt='Urgencias médicas a domicilio Mérida Yucatán'>
 									<br>
 
 							</div>
@@ -184,7 +184,7 @@
 							<div class='col-md-4 col-sm-4'>
 								<div class='feature-center'>
 									<span class='icon'>
-										<img class='img-responsive center-block' src='images/vitality-valoracion-inicial.png' alt='Nuestro personal vitality'>
+										<img class='img-responsive center-block' src='images/vitality-valoracion-inicial.png' alt='Logo Valoración Inicial'>
 									</span>
 									<h2 class='center'>Valoración Inicial</h2>						
 								</div>
@@ -193,7 +193,7 @@
 							<div class='col-md-4 col-sm-4'>
 								<div class='feature-center'>
 									<span class='icon'>
-										<img class='img-responsive center-block' src='images/vitality-mejor personal.png' alt='Nuestro personal vitality'>
+										<img class='img-responsive center-block' src='images/vitality-mejor personal.png' alt='Logo El mejor personal'>
 									</span>
 									<h2 class='center'>El mejor personal</h2>						
 								</div>
@@ -202,7 +202,7 @@
 							<div class='col-md-4 col-sm-4'>
 								<div class='feature-center'>
 									<span class='icon'>
-										<img class='img-responsive center-block' src='images/vitality-supervision-constante.png' alt='Nuestro personal vitality'>
+										<img class='img-responsive center-block' src='images/vitality-supervision-constante.png' alt='Logo Supervisión Constante'>
 									</span>
 									<h2 class='center'>Supervisión Constante</h2>						
 								</div>
@@ -219,7 +219,7 @@
 							<div class='col-md-4 col-sm-4'>
 								<div class='feature-center'>
 									<span class='icon'>
-										<img class='img-responsive center-block' src='images/vitality-garantia.png' alt='Nuestro personal vitality'>
+										<img class='img-responsive center-block' src='images/vitality-garantia.png' alt='Logo Garantía de Cobertura'>
 									</span>
 									<h2 class='center'>Garantía de Cobertura</h2>						
 								</div>
@@ -228,7 +228,7 @@
 							<div class='col-md-4 col-sm-4'>
 								<div class='feature-center'>
 									<span class='icon'>
-										<img class='img-responsive center-block' src='images/vitality-psicoterapia.png' alt='Nuestro personal vitality'>
+										<img class='img-responsive center-block' src='images/vitality-psicoterapia.png' alt='Logo Confianza y Tranquilidad'>
 									</span>
 									<h2 class='center'>Confianza y Tranquilidad</h2>						
 								</div>
