f44704c98648fddcc2bc256dd7d863a88185887c
jnoelopez85 <39996946+jnoelopez85@users.noreply.github.com>
2019-01-16 18:12:22 -0600

    Update contacto-vitality.php

--- contacto-vitality.php
+++ contacto-vitality.php
@@ -100,7 +100,7 @@
 				</div>
 				
 				<div class='col-md-6 animate-box' data-animate-effect='fadeIn'>
-					<h4 class='text-center'>Ubicación en mapa</h4>
+					<h4 class='text-center'>Ubicación en mapa Mérida</h4>
 					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10535.460992654898!2d-89.60545967635932!3d20.99871870065108!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f56712dbaaa47ff%3A0x67f2b53ce42d8af9!2sVitality+M%C3%A9xico!5e0!3m2!1ses-419!2smx!4v1494619675904" width="100%" height="550" frameborder="0" style="border:0" allowfullscreen></iframe>
 				</div>
 				
