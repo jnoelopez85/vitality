045459443f27d3da80739aa4bbaa8a0181af0eb4
jnoelopez85 <39996946+jnoelopez85@users.noreply.github.com>
2019-08-26 20:37:21 -0500

    Add files via upload

--- libvitality.php
+++ libvitality.php
@@ -23,7 +23,7 @@
 		echo"
 			<meta charset='utf-8'>
 			<meta http-equiv='X-UA-Compatible' content='IE=edge'>
-			<link rel='shortcut icon' href='images/vitality.ico'/>
+			<link rel='shortcut icon' href='http://vitalitymexico.com/images/vitality.ico'/>
 			<meta http-equiv='expires' content='86400'/>
 			
 			<title>$titulo</title>
